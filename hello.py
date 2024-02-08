0b42994687893c90261b8583468362afac759c47
Mateusz Ormianek <mattormianek@gmail.com>
2024-02-08 20:37:02 +0100

    fix: changed way of printing progress bar

--- hello.py
+++ hello.py
@@ -1,7 +1,10 @@
 import time
 import numpy as np
 
-def print_progress_bar(iteration, total, length=100, fill="▓", barfill="░", decimals=1):
+
+def print_progress_bar(iteration, total, length=50, fill="█", barfill="░", decimals=2):
+#def print_progress_bar(iteration, total, length=25, fill="⬜", barfill="⬛", decimals=1):
+
     """
     Print progress bar.
 
@@ -14,11 +17,11 @@ def print_progress_bar(iteration, total, length=100, fill="▓", barfill="░", 
     - decimals (int): Precision of percentage (default 2).
     """
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
-    filledLength = int(length * iteration // total)
-    bar = fill * filledLength + barfill * (length - filledLength)
+    filled_length = int(length * iteration // total)
+    bar = fill * filled_length + barfill * (length - filled_length - 1)
     print(f'\r {bar} {percent}%', end='\r')
     if iteration == total:
-        print()
+        print("")
 
 def main():
     avg_time = run_multiple_times(2)
@@ -52,10 +55,14 @@ def run_multiple_times_numpy(number_of_times):
 
 def run_me_loop_and_time_it_with_printing(number_of_times, number_to_calculate):
     start = time.time()
+    counter = 0
     for i in range(number_of_times):
-        print_progress_bar(i, number_of_times)
+        if i%100 == 0:
+            counter += 1
+            print_progress_bar(i, number_of_times)
         calcualte_me_something(i, number_to_calculate)
     end = time.time()
+    print("")
     return end - start
 
 def run_me_loop_and_time_it(number_of_times, number_to_calculate):
